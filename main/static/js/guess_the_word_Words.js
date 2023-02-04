1160c98ff0bbc41d54a4ecbd6db1cff1f4b284d4
Khyati-Parmar <khyati.parmar27@gmail.com>
2023-02-04 17:34:14 +0530

    Added Multiplication and guess game

---
+++ main/static/js/guess_the_word_Words.js
@@ -0,0 +1,94 @@
+let wordList = [
+    {
+        word: "apple",
+        hint: "red friut"
+    },
+    {
+        word: "seven",
+        hint: "Number of days in a week"
+    },
+    {
+        word: "cub",
+        hint: "baby lion"
+    },
+    {
+        word: "aeiou",
+        hint: "vowels"
+    },
+    {
+        word: "mango",
+        hint: "national fruit of india"
+    },
+    {
+        word: "four",
+        hint: "number of colours in indian national flag"
+    },
+    {
+        word: "canada",
+        hint: "name of country"
+    },
+    {
+        word: "asia",
+        hint: "biggest continent in the world "
+    },
+    {
+        word: "igloo",
+        hint: "house made of ice"
+    },
+    {
+        word: "east",
+        hint: "sun rises in this direction"
+    },
+    {
+        word: "26",
+        hint: "number of alphabets "
+    },
+    {
+        word: "blue ",
+        hint: "colour of sky"
+    },
+    {
+        word: "February",
+        hint: "smallest month of year"
+    },
+    {
+        word: "holi",
+        hint: "festival of colours "
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },    
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },   
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },   
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "winter",
+        hint: "season of cold"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
